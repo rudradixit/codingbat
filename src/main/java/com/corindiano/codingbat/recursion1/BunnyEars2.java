f1e5475030814bbbbd421fcefd25146bd2f1bcf2
Rudra A Dixit <rudradixit@gmail.com>
2018-08-30 13:44:49 -0400

    Adding first couple of solutions

---
+++ src/main/java/com/corindiano/codingbat/recursion1/BunnyEars2.java
@@ -0,0 +1,23 @@
+package com.corindiano.codingbat.recursion1;
+
+/**
+ * We have bunnies standing in a line, numbered 1, 2, ...
+ * The odd bunnies (1, 3, ..) have the normal 2 ears.
+ * The even bunnies (2, 4, ..) we'll say have 3 ears,
+ * because they each have a raised foot.
+ *
+ * Recursively return the number of "ears" in the bunny line 1, 2, ... n
+ * (without loops or multiplication).
+ *
+ * bunnyEars2(0) → 0
+ * bunnyEars2(1) → 2
+ * bunnyEars2(2) → 5
+ */
+class BunnyEars2 {
+    int bunnyEars2(int bunnies) {
+        if (bunnies == 0) return 0;
+
+        int ears = bunnies % 2 == 0 ? 3 : 2;
+        return ears + bunnyEars2(bunnies - 1);
+    }
+}
